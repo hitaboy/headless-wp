1874e6f0a5715e538ca004cf6b7768802475d75c
Pere Esteve <pereesteve@MacBook-Pro.local>
2020-09-27 00:05:20 +0200

    wpauop filter disabled and ACF fields exposing for WP Rest API

--- README.md
+++ README.md
@@ -13,7 +13,7 @@ Version: 1.0
 ```
 
 ## index.php
-Meant to don't execute any wp or ph function. Just a plain html. 
+Meant to don't execute any wp or ph function. Just a plain html.
 ```
 <html>
   <head>
@@ -25,4 +25,36 @@ Meant to don't execute any wp or ph function. Just a plain html.
 </html>
 
 ```
+## functions.php
+Wpautop is the name of the function that automatically encloses double-line breaks with a <p> and </ p>. WordPress uses this feature in all post types (posts, pages and custom post types)- for both content and shortcode generated texts. Hence, some of the editing jobs such as working with a shortcode-driven grid system in the WYSIWYG editor can be quite problematic.
+The wpautop filter can be disabled with this line.
+```
+remove_filter( 'the_content', 'wpautop' );
+```
 
+If using ACF plugin and having the need to get the fields trought the WP REST API, use this code that automatically expose all the ACF fields to the Wordpress REST API in Pages and in your custom post types.
+```
+function create_ACF_meta_in_REST() {
+    $postypes_to_exclude = ['acf-field-group','acf-field'];
+    $extra_postypes_to_include = ["page"];
+    $post_types = array_diff(get_post_types(["_builtin" => false], 'names'),$postypes_to_exclude);
+
+    array_push($post_types, $extra_postypes_to_include);
+
+    foreach ($post_types as $post_type) {
+        register_rest_field( $post_type, 'ACF', [
+            'get_callback'    => 'expose_ACF_fields',
+            'schema'          => null,
+       ]
+     );
+    }
+
+}
+
+function expose_ACF_fields( $object ) {
+    $ID = $object['id'];
+    return get_fields($ID);
+}
+
+add_action( 'rest_api_init', 'create_ACF_meta_in_REST' );
+```
--- functions.php
+++ functions.php
@@ -1,29 +1,37 @@
 <?php
-
+/*
+DISABLE WPAUTOP
+Wpautop is the name of the function that automatically encloses double-line breaks with a <p> and </ p>. WordPress uses this feature in all post types (posts, pages and custom post types)- for both content and shortcode generated texts. Hence, some of the editing jobs such as working with a shortcode-driven grid system in the WYSIWYG editor can be quite problematic.
+The wpautop filter can be disabled with this line.
+*/
 remove_filter( 'the_content', 'wpautop' );
 
+/*
+ACF FIELDS TO WP REST API
+Automatically expose all the ACF fields to the Wordpress REST API in Pages and in your custom post types.
+*/
 
-    function create_ACF_meta_in_REST() {
-        $postypes_to_exclude = ['acf-field-group','acf-field'];
-        $extra_postypes_to_include = ["page"];
-        $post_types = array_diff(get_post_types(["_builtin" => false], 'names'),$postypes_to_exclude);
+function create_ACF_meta_in_REST() {
+    $postypes_to_exclude = ['acf-field-group','acf-field'];
+    $extra_postypes_to_include = ["page"];
+    $post_types = array_diff(get_post_types(["_builtin" => false], 'names'),$postypes_to_exclude);
 
-        array_push($post_types, $extra_postypes_to_include);
+    array_push($post_types, $extra_postypes_to_include);
 
-        foreach ($post_types as $post_type) {
-            register_rest_field( $post_type, 'ACF', [
-                'get_callback'    => 'expose_ACF_fields',
-                'schema'          => null,
-           ]
-         );
-        }
-
+    foreach ($post_types as $post_type) {
+        register_rest_field( $post_type, 'ACF', [
+            'get_callback'    => 'expose_ACF_fields',
+            'schema'          => null,
+       ]
+     );
     }
 
-    function expose_ACF_fields( $object ) {
-        $ID = $object['id'];
-        return get_fields($ID);
-    }
+}
 
-    add_action( 'rest_api_init', 'create_ACF_meta_in_REST' );
+function expose_ACF_fields( $object ) {
+    $ID = $object['id'];
+    return get_fields($ID);
+}
+
+add_action( 'rest_api_init', 'create_ACF_meta_in_REST' );
 ?>
